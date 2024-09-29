7bab10ac8fdd806978ea27c4752bb5b34287bc9b
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2024-09-29 10:19:45 +0800

    统一脚本

--- Runtime/Localization/Component/LocalizationText.cs
+++ Runtime/Localization/Component/LocalizationText.cs
@@ -4,13 +4,12 @@
  *UnityVersion:   2021.3.33f1c1
  *Date:           2024-04-25
 *********************************************************************************/
-using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace WooLocalization
 {
+
 
     [UnityEngine.RequireComponent(typeof(UnityEngine.UI.Text))]
     [DisallowMultipleComponent]
@@ -35,75 +34,11 @@ namespace WooLocalization
             }
         }
         [System.Serializable]
-        public class TextValueActor : LocalizationActor<LocalizationText>
+        public class TextValueActor : TextValueActor_Base<LocalizationText>
         {
-            public string key;
-            private string _lastKey;
-            public string[] formatArgs = new string[0];
-
-            public TextValueActor(bool enable) : base(enable)
-            {
-
-            }
-            protected override void OnAddComponent()
-            {
-                if (string.IsNullOrEmpty(key))
-                {
-
-                    var txt = this.behavior.graphicT.text;
-                    if (!string.IsNullOrEmpty(txt))
-                    {
-                        var contxt = this.behavior.context;
-                        if (contxt != null)
-                        {
-                            var key = contxt.FindKey(Localization.localizationType, txt);
-                            SetKey(key);
-                        }
-                    }
-                }
-            }
-            private static Regex regex = new Regex("^{[0-9]*}$");
-
-            public string GetTargetText(LocalizationBehavior component, out Exception err)
-            {
-                err = null;
-                var format = component.GetLocalization(key);
-                if (regex.Match(format) == null) return format;
-                try
-                {
-                    return string.Format(format, formatArgs);
-                }
-                catch (System.Exception ex)
-                {
-                    err = ex;
-                    return format;
-                }
-            }
-            protected override void Execute(string localizationType, LocalizationText component)
-            {
-                _lastKey = key;
-                Exception err;
-                component.graphicT.text = GetTargetText(component, out err);
-                if (err != null)
-                    throw err;
-
-            }
-            public void SetKey(string key)
-            {
-                this.key = key;
-                ((ILocalizationActor)this).enable = true;
-                ((ILocalizationActor)this).Execute();
-            }
-            protected override bool NeedExecute(string localizationType)
-            {
-                var _base = base.NeedExecute(localizationType);
-                bool self = _lastKey != this.key;
-#if UNITY_EDITOR
-                if (!Application.isPlaying)
-                    self = true;
-#endif
-                return self || _base;
-            }
+            public TextValueActor(bool enable) : base(enable) { }
+            protected override string GetComponentText() => this.behavior.graphicT.text;
+            protected override void SetComponentText(string value) => this.behavior.graphicT.text = value;
         }
         [System.Serializable]
         public class TextFontSizeActor : LocalizationMapActor<LocalizationText, int>
