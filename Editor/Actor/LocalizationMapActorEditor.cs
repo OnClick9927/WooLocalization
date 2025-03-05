fc944c2a86881a528170a5126efc52b3db968e9e
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2025-03-05 17:22:36 +0800

    支持运行时设置自定义 获取  图片/特效等 本地化方式

--- Editor/Actor/LocalizationMapActorEditor.cs
+++ Editor/Actor/LocalizationMapActorEditor.cs
@@ -5,6 +5,9 @@
  *Date:           2024-04-25
 *********************************************************************************/
 
+using UnityEditor;
+using UnityEngine;
+
 namespace WooLocalization
 {
     public abstract class LocalizationMapActorEditor<Actor, Value, Behavior> : LocalizationActorEditor<Actor>
@@ -14,6 +17,15 @@ namespace WooLocalization
         protected override void OnGUI(LocalizationBehavior component, Actor context)
         {
             if (component.context == null) return;
+            GUI.enabled = false;
+            EditorGUILayout.EnumPopup(nameof(context.mode), context.mode);
+            if (context.mode != LocalizationMapActor<Behavior, Value>.Mode.Default)
+            {
+                EditorGUILayout.TextField(nameof(context.key), context.key);
+                EditorGUILayout.TextField(nameof(context.CustomContextType), context.CustomContextType);
+            }
+
+            GUI.enabled = true;
             var keys = component.GetLocalizationTypes();
             var map = context.map;
             for (int i = 0; keys.Count > i; i++)
@@ -24,10 +36,13 @@ namespace WooLocalization
                     SetDirty(component);
                 }
             }
+            if (context.mode != LocalizationMapActor<Behavior, Value>.Mode.Default)
+                return;
             for (int i = 0; i < keys.Count; i++)
             {
                 var lan = keys[i];
                 var src = map[lan];
+
                 var tmp = Draw(lan, src);
                 bool change = false;
                 if (src == null)
--- Runtime/Actor/LocalizationMapActor.cs
+++ Runtime/Actor/LocalizationMapActor.cs
@@ -4,28 +4,82 @@
  *UnityVersion:   2021.3.33f1c1
  *Date:           2024-04-25
 *********************************************************************************/
+using UnityEngine;
+
 namespace WooLocalization
 {
+
     public abstract class LocalizationMapActor<Behavior, Value> : LocalizationActor<Behavior> where Behavior : LocalizationBehavior
     {
-        public SerializableDictionary<string, Value> map = new SerializableDictionary<string, Value>();
+        [System.Serializable]
+        public class MapContext : IMapActorContext<Value>
+        {
+            internal SerializableDictionary<string, Value> map => actor.map;
+            internal LocalizationMapActor<Behavior, Value> actor;
+            public Value GetValue(string localizationType, string key)
+            {
+                Value v;
+                map.TryGetValue(localizationType, out v);
+                return v;
+            }
 
+            internal bool AddLocalizationTypeToMap(string localizationType, Value _default)
+            {
+                Value value;
+                if (!map.TryGetValue(localizationType, out value))
+                {
+                    map.Add(localizationType, _default);
+                    return true;
+                }
+                return false;
+            }
+        }
+        public enum Mode
+        {
+            Default, Custom
+        }
+        public SerializableDictionary<string, Value> map = new SerializableDictionary<string, Value>();
+        public Mode mode { get; private set; }
+        public string key { get; private set; }
+
+
+        [SerializeField] private MapContext context = new MapContext();
+        private IMapActorContext<Value> customContext;
+        public string CustomContextType { get; private set; }
+        public void SetCustomContext(IMapActorContext<Value> context, string key)
+        {
+            customContext = context;
+            if (context != null)
+            {
+                CustomContextType = context.GetType().Name;
+                mode = Mode.Custom;
+                this.key = key;
+            }
+        }
         protected LocalizationMapActor(bool enable) : base(enable)
         {
+            context.actor = this;
+        }
+        protected override void OnAddComponent()
+        {
+            context.actor = this;
+            base.OnAddComponent();
         }
         public bool AddLocalizationTypeToMap(string localizationType)
         {
-            Value value;
-            if (!map.TryGetValue(localizationType, out value))
-            {
-                map.Add(localizationType, GetDefault());
-                return true;
-            }
+#if UNITY_EDITOR
+            if (Application.isPlaying)
+                return false;
+            return context.AddLocalizationTypeToMap(localizationType, GetDefault());
+#endif
             return false;
         }
         protected sealed override void BeforeExecute(string localizationType)
         {
-            AddLocalizationTypeToMap(localizationType);
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                AddLocalizationTypeToMap(localizationType);
+#endif
         }
         protected virtual Value GetDefault() => default;
         public Value GetValue()
@@ -34,9 +88,9 @@ namespace WooLocalization
         }
         public Value GetValue(string localizationType)
         {
-            Value v;
-            map.TryGetValue(localizationType, out v);
-            return v;
+            if (mode == Mode.Custom)
+                return customContext.GetValue(localizationType, key);
+            return context.GetValue(localizationType, key);
         }
     }
 }
