bbbb9cdee35a276090efada5dd2ebb11f70f01fc
Ling Wen <54854040+OnClick9927@users.noreply.github.com>
2025-06-19 14:15:14 +0800

    加俩扩展

---
+++ Assets/Exend/LocalizationEx.cs
@@ -0,0 +1,22 @@
+﻿/*********************************************************************************
+ *Author:         OnClick
+ *Version:        0.1
+ *UnityVersion:   2021.3.33f1c1
+ *Date:           2024-04-25
+*********************************************************************************/
+using TMPro;
+
+namespace WooLocalization
+{
+    public static class LocalizationEx
+    {
+        public static void SetLocalization(this TextMeshProUGUI tmp, string key, params string[] args)
+        {
+            var comp = tmp.GetComponent<WooLocalization.LocalizationTMP_Text>();
+            if (comp == null)
+                comp = tmp.gameObject.AddComponent<WooLocalization.LocalizationTMP_Text>();
+            comp.text.formatArgs = args;
+            comp.text.SetKey(key);
+        }
+    }
+}
---
+++ Assets/WooLocalization/Runtime/LocalizationEx.cs
@@ -0,0 +1,22 @@
+﻿/*********************************************************************************
+ *Author:         OnClick
+ *Version:        0.1
+ *UnityVersion:   2021.3.33f1c1
+ *Date:           2024-04-25
+*********************************************************************************/
+using UnityEngine.UI;
+
+namespace WooLocalization
+{
+    public static class LocalizationEx
+    {
+        public static void SetLocalization(this Text tmp, string key, params string[] args)
+        {
+            var comp = tmp.GetComponent<WooLocalization.LocalizationText>();
+            if (comp == null)
+                comp = tmp.gameObject.AddComponent<WooLocalization.LocalizationText>();
+            comp.text.formatArgs = args;
+            comp.text.SetKey(key);
+        }
+    }
+}
